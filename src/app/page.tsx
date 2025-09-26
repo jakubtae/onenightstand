9aa8be8ac922397237d9f17ea0cb90c9bd569207
Jakub <jakub.tomczyk.2005@gmail.com>
2025-09-26 22:33:20 +0200

    montserrat font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-6xl font-bold">Hate your life?</h1>
+    <div className="font-montserrat flex flex-col items-center min-h-screen p-2 pt-8 gap-2 sm:p-20 bg-gray-100 border border-gray-300 w-full rounded-t-4xl mt-20">
+      <h1 className="text-6xl lg:text-9xl font-bold uppercase text-center tracking-widest xl:tracking-[5rem] lg:tracking-[3rem] md:tracking-[1.5rem] ">
+        Lock in
+      </h1>
+      <h2 className="text-2xl lg:text-4xl font-medium  text-gray-900">
+        In a rut? A depressive episode?
+      </h2>
       <Button variant="default">
-        <a href="/start" className="font-semibold">
-          Let&apos;s fix it
-        </a>
+        <Link href="/start" className="font-semibold">
+          Just lock in
+        </Link>
       </Button>
     </div>
   );
